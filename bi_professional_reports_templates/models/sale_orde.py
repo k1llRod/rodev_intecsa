2cade0a2ce0ba241b5ca1badd91eae67fde9b904
Rodrigo <kurt.gf@gmail.com>
2023-10-25 23:32:56 -0400

    [INIT]add addons

---
+++ bi_professional_reports_templates/models/sale_orde.py
@@ -0,0 +1,10 @@
+# Copyright 2014-2020 Tecnativa - Pedro M. Baeza
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models
+
+
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+
+    vr_print_line = fields.Boolean(string="Imp.", default=True)
